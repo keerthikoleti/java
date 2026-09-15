1dc4be0b2de19a789283c9418c5dae470c5096ef
koleti keerthi <keerthikoleti582@gmail.com>
2026-09-15 18:36:51 +0530

    Create InputBox.java

---
+++ InputBox.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class InputBox
+{
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(
+            null,
+            "hello boys & girls",
+            "FirstSwingProgram",
+            JOptionPane.QUESTION_MESSAGE
+        );
+    }
+}
