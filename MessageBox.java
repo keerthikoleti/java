386f03633f45f0137b3688f4db78db3ccb8daaa3
koleti keerthi <keerthikoleti582@gmail.com>
2026-09-15 18:28:12 +0530

    Create MessageBox.java

---
+++ MessageBox.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class MessageBox
+{
+    public static void main(String[] args)
+    {
+        JOptionPane.showMessageDialog(
+            null,
+            "hello boys & girls",
+            "FirstSwingProgram",
+            JOptionPane.QUESTION_MESSAGE
+        );
+    }
+}
